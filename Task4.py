d60e8a9b62b96c39fba65102c67a3ad23c8d203d
4YGYN <64703276+4YGYN@users.noreply.github.com>
2020-07-30 11:25:55 +0300

    Чувствую, дальше будет куда сложнее

---
+++ Task4.py
@@ -0,0 +1,84 @@
+"""
+Реализуйте базовый класс Car.
+У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево).
+А также методы: go, stop, turn(direction), которые должны сообщать, что машина поехала, остановилась, повернула (куда).
+Опишите несколько дочерних классов: TownCar, SportCar, WorkCar, PoliceCar.
+Добавьте в базовый класс метод show_speed, который должен показывать текущую скорость автомобиля.
+Для классов TownCar и WorkCar переопределите метод show_speed.
+При значении скорости свыше 60 (TownCar) и 40 (WorkCar) должно выводиться сообщение о превышении скорости.
+"""
+
+
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        return f'{self.name} поехала'
+
+    def stop(self):
+        return f"{self.name} остановилась"
+
+    def turn_right(self):
+        return f'{self.name} повернула направо'
+
+    def turn_left(self):
+        return f'{self.name} поеврнула налево'
+
+    def show_speed(self):
+        return f'Скорость {self.name} - {self.speed}'
+
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Скорость {self.name} составляет {self.speed}')
+
+        if self.speed > 60:
+            return f'{self.name} превышает скорость'
+        else:
+            return f'{self.name} не превышает'
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Скорость {self.name} составляет {self.speed}')
+
+        if self.speed > 40:
+            return f'{self.name} превышает скорость'
+        else:
+            return f'{self.name} не превышает'
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def police(self):
+        if self.is_police:
+            return f'{self.name} из полиции'
+        else:
+            return f'{self.name} не из полиции'
+
+audi = SportCar(100, 'Red', 'Audi', False)
+oka = TownCar(70, 'White', 'Oka', False)
+lada = WorkCar(70, 'Rose', 'Lada', True)
+ford = PoliceCar(110, 'Blue',  'Ford', True)
+print(lada.turn_left())
+print(audi.show_speed())
+print(lada.show_speed())
+print(oka.show_speed())
+print(ford.police())
+print(ford.show_speed())
